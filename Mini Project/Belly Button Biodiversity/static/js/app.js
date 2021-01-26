f3d4a31fe22b9b52b7e1897eeb2e3adc0e931b3d
Brooke Cooper <techanalyst65@gmail.com>
2021-01-26 11:18:00 -0500

    Uploading latest updates

--- Mini Project/Belly Button Biodiversity/static/js/app.js
+++ Mini Project/Belly Button Biodiversity/static/js/app.js
@@ -10,60 +10,135 @@ console.log(reversedTextArray);
 
 function populateSelect() {
 
+  // Use D3 to capture the JSON data from the URL
+  // which yields a nested object of arrays of objects
+  // We also checked the data directly in Chrome with JSON viewer extension
   d3.json(url).then(function(samplesData) {
 
+    // Print / test the entire dataset to the console
+    // This shows 3 arrays each with 153 items
+    // One (names) is a simple array with values
+    // The other two (metadata, samples) are arrays of objects
+    // The samples array is an array of objects of arrays
+    console.log("--- testing Entire Dataset aka samplesData ---");
     console.log(samplesData);
 
-    var subjectIds3 = samplesData['names'];
-    console.log("testing subjectIds3");
-    console.log(subjectIds3);    
+    console.log("--- testing metadata ---");
+    console.log(samplesData.metadata);
 
-    var subjectIds2 = Object.values(samplesData)[0][0];
-    console.log("testing subjectIds2");
-    console.log(subjectIds2);
-    
-    var subjectIds = samplesData(p => p.names);
-    console.log("testing subjectIds");
-    console.log(subjectIds);
+    console.log("--- testing samples ---");
+    console.log(samplesData.samples);
+
+    var subjectIds = samplesData['names'];
+    console.log("--- testing subjectIds ---");
+    console.log(subjectIds);    
 
     // Use D3 to select / alias the selector button and option field
-    var selectList = d3.select('selDataset');
+    var selectList = d3.select('#selDataset');
 
-    // Iterate through each object in an array of objects
-    subjectIds3.forEach(item => {      
+    // Iterate through each object 
+    subjectIds.forEach(item => {      
 
+      // Create a new option (drop down tag item)
+      // which is appended to the drop down selector 
       var option = selectList.append('option');
-
-      option.append('option').text(item['names']);
-      option.append('option').value(item['names']);      
-
+      
+      // Copy the subjectID value to the option tag's visible text 
+      // field and to the option tag's value field (hidden from view 
+      // but used to query the dataset and retrieve the selected record)
+      // I had mistakenly tried to use the append method here.  Doh!
+      option.text(item).property("value", item);
+    });
   });
-});
+}
 
 populateSelect();
 
 
-    
-    // var subjectIds = Object.values(samplesData)[1];
-    
-/*
-    var ele = document.getElementById('selDataset');
-    for (var i = 0; i < subjectIds.length; i++) {
-    
-      // Bind data to <select> element.
-      ele.innerHTML = ele.innerHTML +
-        '<option value="' + Object.values(subjectIds)[i] + '</option>';
-*/
+
+
+d3.json(url).then(function(samplesData) {
+
+
+  var selectElement = document.getElementById("selDataset");
+  var selectRefinedElement = selectElement.getElementsByClassName("option");
+  var selectValue = selectRefinedElement.text;
+
+  // var selectElement = d3.select('#selDataset>option');
+  // var selectValue = selectElement.property('value');
+  console.log("--- testing selectValue ---");
+  console.log(`selectValue = ${selectValue}`);
+
+  var samplesMetadata = samplesData.metadata
+  var individualMetadata = samplesMetadata.filter(obj => obj.id == 940);
+  var individualWfreq = individualMetadata.wfreq
+  console.log("--- testing individualMetadata ---");
+  console.log(individualMetadata);
+  console.log("--- testing individualWfreq ---");
+  console.log(individualWfreq);
+
+  var assaysSamplesdata = samplesData.samples
+  var individualSampledata = assaysSamplesdata.filter(obj => obj.id == 940);
+  console.log("--- testing individualSampledata ---");
+  console.log(individualSampledata);  
+
+
+  const data = {
+    5: ['3','1','2'],
+    8: ['E','F','D']
+   }
+   
+   let dataSort = Object.keys(data).reduce((acc, item) => {
+     acc[item] = data[item].sort()  
+     return acc
+   }, {})
+   
+   console.log(dataSort)
+
+});
+
+
+
   
   
   
 
 
-/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Identify web element(s) on the page
-selectbtn = d3.select('#selDataset');
+// selectbtn = d3.select('#selDataset');
 
 // Add event listeners to the web elements
 // selectbtn.on('change', FUNCTION_TBD);
-*/
